34fdd739efbd3807f4051495c9b355955ff161ae
Marcelo Augusto da Silva Leite <marcelomagu2006@gmail.com>
2025-10-16 20:59:23 -0300

    Atualiza estilos para roxo

--- script.js
+++ script.js
@@ -3,14 +3,26 @@ const usuario = document.getElementById("usuario");
 const senha = document.getElementById("senha");
 const mensagem = document.getElementById("mensagem");
 form.addEventListener("submit", (e) => {
+<<<<<<< HEAD
  e.preventDefault(); // evita o recarregamento da página
  
  if (usuario.value === "" || senha.value === "") {
  mensagem.textContent = "Preencha todos os campos!";
  } else if (usuario.value === "adm" && senha.value === "123") {
- mensagem.style.color = "#03dac6";
+ mensagem.style.color = "#4e03daff";
  mensagem.textContent = "Login realizado com sucesso!";
  } else {
  mensagem.textContent = "Usuário ou senha incorretos.";
  }
+=======
+e.preventDefault(); // evita o recarregamento da página
+if (usuario.value === "" || senha.value === "") {
+mensagem.textContent = "Preencha todos os campos!";
+} else if (usuario.value === "adm" && senha.value === "123") {
+mensagem.style.color = "#6826a7ff";
+mensagem.textContent = "Login realizado com sucesso!";
+} else {
+mensagem.textContent = "Usuário ou senha incorretos.";
+}
+>>>>>>> develop
 });
