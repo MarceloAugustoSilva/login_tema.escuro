6e3ec40a023314601df8a52ab91ca2f83aff7f31
Marcelo Augusto da Silva Leite <marcelomagu2006@gmail.com>
2025-10-16 20:39:57 -0300

    alteração de cores

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ e.preventDefault(); // evita o recarregamento da página
 if (usuario.value === "" || senha.value === "") {
 mensagem.textContent = "Preencha todos os campos!";
 } else if (usuario.value === "adm" && senha.value === "123") {
-mensagem.style.color = "#03dac6";
+mensagem.style.color = "#6826a7ff";
 mensagem.textContent = "Login realizado com sucesso!";
 } else {
 mensagem.textContent = "Usuário ou senha incorretos.";
